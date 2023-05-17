1710697a2026de12392bb4f65e276928ef175534
namkhenh <namkhenh81@gmail.com>
2023-05-17 15:12:05 +0700

    first commit

---
+++ Portal.API/Rpc/menu/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portal.API.Rpc.menu
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MenuController : RpcController
+  {
+    [Route(MenuRoot.Get), HttpPost]
+    public IActionResult Demo()
+    {
+      return Ok();
+    }
+  }
+}
